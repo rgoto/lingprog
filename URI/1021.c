5c9617559854ce0209602e1b94e28bc706348479
Rafael Goto <rafael.fgoto@outlook.com>
2016-04-10 15:50:18 -0300

    Exercicios URI

--- URI/1021.c
+++ URI/1021.c
@@ -1,30 +1,50 @@
-#include <stdio.h>
-int main (void){
-    int c100, c50, c20, c10, c5, c2 ,c1;
-    float v, m1, m5, m25, m10, m05, m01;
-        scanf ("%d", &v);
-            printf("NOTAS:");
-
-        c100=v/100;
-            printf("%d nota(s) de R$ 100,00\n", c100);
-                v=v%100;
-                c50=v/50;
-                    printf("%d nota(s) de R$ 50,00\n", c50);
-                        v=v%50;
-                        c20=v/20;
-                            printf("%d nota(s) de R$ 20,00\n", c20);
-                            v=v%20;
-                            c10=v/10;
-                                printf("%d nota(s) de R$ 10,00\n", c10);
-                            v=v%10;
-                             c5=v/5;
-                        printf("%d nota(s) de R$ 5,00\n", c5);
-                    v=v%5;
-                    c2=v/2;
-                printf("%d nota(s) de R$ 2,00\n", c2);
-            v=v%2;
-            c1=v/1;
-        printf("%d nota(s) de R$ 1,00\n", c1);
-
-
-}
+#include <stdio.h>  
+int main(void)  {  
+    
+    double A,D;  
+        scanf ("%lf",&A);  
+            int N,a,b,c,d,e,f,g,h,i,j,k,l,B,m,n,o,p,q,r,s,t,u,E;  
+    
+    N=A;  
+    a=N/100;  
+    b=N%100;  
+    c=b/50;  
+    d=b%50;  
+    e=d/20;  
+    f=d%20;  
+    g=f/10;  
+    h=f%10;  
+    i=h/5;  
+    j=h%5;  
+    k=j/2;  
+    l=j%2;  
+  
+    E=A*100;  
+    B=E;  
+    m=B%100;  
+    n=m/50;  
+    o=m%50;  
+    p=o/25;  
+    q=o%25;  
+    r=q/10;  
+    s=q%10;  
+    t=s/5;  
+    u=s%5;  
+  
+    printf ("NOTAS:\n");  
+    printf ("%d nota(s) de R$ 100.00\n",a);  
+    printf ("%d nota(s) de R$ 50.00\n",c);  
+    printf ("%d nota(s) de R$ 20.00\n",e);  
+    printf ("%d nota(s) de R$ 10.00\n",g);  
+    printf ("%d nota(s) de R$ 5.00\n",i);  
+    printf ("%d nota(s) de R$ 2.00\n",k);  
+    printf ("MOEDAS:\n");  
+    printf ("%d moeda(s) de R$ 1.00\n",l);  
+    printf ("%d moeda(s) de R$ 0.50\n",n);  
+    printf ("%d moeda(s) de R$ 0.25\n",p);  
+    printf ("%d moeda(s) de R$ 0.10\n",r);  
+    printf ("%d moeda(s) de R$ 0.05\n",t);  
+    printf ("%d moeda(s) de R$ 0.01\n",u);  
+  
+    return 0;  
+}  
